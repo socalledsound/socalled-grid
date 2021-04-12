ff09f2b5b6437efb2b4d0cd6a42054b8780aa88a
Chris Kubick <socalledsound@gmail.com>
2021-04-12 11:23:50 -0700

    working on nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { AppBar, Toolbar } from "@material-ui/core"
+import { Link } from 'react-router-dom'
+import { makeStyles, AppBar, Toolbar, IconButton, List, ListItem, ListItemText  } from "@material-ui/core"
+import { Home } from "@material-ui/icons"
 
 const navLinks = [
     { title : 'socalledsound', path: '/'},
@@ -10,7 +12,20 @@ const navLinks = [
 const Header = () => {
     return (
         <AppBar position = "static">
-            <Toolbar>hi</Toolbar>
+            <Toolbar>
+                <IconButton edge="start" color="inherit" aria-label="home">
+                    <Home fontSize="large" />
+                </IconButton>
+                <List component="nav" aria-labelledby="main navigation">
+                    {navLinks.map(({ title, path }) => (
+                    <Link to={path} key={title}>
+                        <ListItem button>
+                        <ListItemText primary={title} />
+                        </ListItem>
+                    </Link>
+                    ))}
+                </List>
+            </Toolbar>
         </AppBar>
     )
 }
