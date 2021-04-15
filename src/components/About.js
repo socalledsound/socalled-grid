fb7f5ecad0f328bff233322e53017f1d4b40a087
Chris Kubick <socalledsound@gmail.com>
2021-04-14 21:18:43 -0700

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -11,13 +11,19 @@ const About = () => {
                 that featured Dennis Hopper as a time traveling kung fu warrior in 25th century Shanghai.
                 And also, Futurama, and a few other watchable things.</p>  
                 <p>Somewhere along the way I got a little
-                bored with that and starting coding sounds, and started having museum shows.</p>  
-                <p>Nowadays I mostly
-                build websites, often with a strong audio component.  My favorite tools are web audio, webGL and SVG,
-                alongside a variety of javascript frameworks, but mostly people hire me to build elegant interfaces
-                using React/Redux and svg.</p>  
-                <p>If you want to chat about sound effects, or coding audio for the web,
-                or something else interesting, drop me a line via the contact link in the header!  
+                bored with that and starting coding sounds, and started having museum shows.</p> 
+                <p>Before I knew it I was kind of obsessed with coding.  Now I'm kinda sorta expert not only in the web languages -- HTML/CSS/Javascript,
+                --  but also pretty good in Python, C, and GLSL.  And I still write some stuff in my first love, SuperCollider, too.</p> 
+                <p>For the last decade, I've been a freelance web and app developer.  I also teach creative coding at UC Berkeley and SFAI, 
+                    as well as the occasional guest stint elsewhere.  And, I teach React and other web tech to aspiring developers through my own site 
+                    <a href="https://www.socalledsidefx.com">socalledsidefx.com</a>.
+                </p>
+                <p>I code web sites, games, data visualization, microcontrollers and audio apps, but if I have a particular specialty, it's building
+                    dynamic web apps using React, Redux web audio and SVG.</p>  
+                <p>I also built a sailboat that makes sounds!</p>    
+                <p>If you want to chat about sound effects, coding audio for the web,
+                    or other even more interesting things, or if you want to come hear some sounds on my boat, 
+                    feel free to drop me a line via the contact link in the header!  
             </p>
         </div>
      );
